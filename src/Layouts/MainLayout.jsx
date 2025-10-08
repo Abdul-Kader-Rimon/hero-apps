3c635e69d730adfa19e64e5c4c8cc828bc479064
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-10-08 20:16:57 +0600

    bannar section done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -4,15 +4,17 @@ import { Outlet } from 'react-router';
 import Footer from '../Components/Footer';
 
 const MainLayout = () => {
-    return (
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <div className="max-w-screen-2xl mx-auto w-full px-4 md:px-8 lg:px-12 py-4 md:py-8 lg:py-12 flex-1">
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <div className=" min-h-screen bg-gray-100">
+        <div className="max-w-screen-2xl mx-auto w-full px-4 md:px-8 lg:px-12 py-4 md:py-8 lg:py-12 flex-1 ">
           <Outlet />
         </div>
-        <Footer />
       </div>
-    );
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/Pages/Bannar.jsx
+++ src/Pages/Bannar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import appleStoreLogo from "../assets/applestore.png"
 import playStoreLogo from "../assets/playstore.png"
+import heroImage from "../assets/hero.png"
 const Bannar = () => {
   return (
-    <div>
+    <div className="">
       <div>
         <div className="hero bg-base-200  py-8 md:my-8 lg:py-20 ">
           <div className="hero-content text-center px-4 md:px-8 lg:px-16">
@@ -37,6 +38,40 @@ const Bannar = () => {
                   <img className="h-8 w-8'" src={appleStoreLogo} alt="" />
                   App Store
                 </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <img className="mx-auto" src={heroImage} alt="" />
+        </div>
+        <div className=" lg:absolute w-full lg:left-0 lg:right-0 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] flex items-center justify-center py-16 px-6">
+          <div>
+            <h2 className="text-3xl md:text-4xl font-semibold mb-12 text-center text-white">
+              Trusted by Millions, Built for You
+            </h2>
+            <div className="grid grid-rows-1 md:grid-cols-3 lg:grid-cols-3 gap-10">
+              <div className="lg:p-8 text-center text-white">
+                <p className="text-sm mb-4 opacity-50">Total Downloads</p>
+                <h1 className="text-6xl font-bold mb-4">29.6M</h1>
+                <p className="text-sm mb-4 opacity-50">
+                  21% more than last month
+                </p>
+              </div>
+              <div className="lg:p-8 text-center text-white">
+                <p className="text-sm mb-4 opacity-50">Total Reviews</p>
+                <h1 className="text-6xl font-bold mb-4">906K</h1>
+                <p className="text-sm mb-4 opacity-50">
+                  46% more than last month
+                </p>
+              </div>
+              <div className="lg:p-8 text-center text-white">
+                <p className="text-sm mb-4 opacity-50">Active Apps</p>
+                <h1 className="text-6xl font-bold mb-4">132+</h1>
+                <p className="text-sm mb-4 opacity-50">31 more will Launch</p>
               </div>
             </div>
           </div>
